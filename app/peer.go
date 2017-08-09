b8a51f2fb98fa3cc3e7378fe6079931475a398f2
Toon Sevrin <twan123@live.be>
2017-08-09 17:36:51 +0200

    Fixed uri setup

--- app/peer.go
+++ app/peer.go
@@ -1,10 +1,11 @@
 package app
 
+import "strconv"
 type Peer struct {
 	Ip string `json:"ip"`
 	Port int `json:"port"`
 }
 
 func (peer *Peer) getUrl() string{
-	return "http://" + peer.Ip + ":" + string(peer.Port)
+	return "http://" + peer.Ip + ":" + strconv.Itoa(peer.Port)
 }
